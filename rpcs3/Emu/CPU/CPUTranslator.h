f2ddc9d8c8d8c063ef303a99354aa6b3fc99c793
ZEROx <Autoran1@yandex.ru>
2024-12-26 21:08:39 +0300

    Revert "Add various SPU patterns"
    This reverts commit 65d93c97ea75a1ddeada3ba3486b9937c1951ff9.
    
    # Conflicts:
    #	rpcs3/Emu/Cell/SPULLVMRecompiler.cpp

--- rpcs3/Emu/CPU/CPUTranslator.h
+++ rpcs3/Emu/CPU/CPUTranslator.h
@@ -43,9 +43,6 @@
 #include <functional>
 #include <unordered_map>
 
-// Helper function
-llvm::Value* peek_through_bitcasts(llvm::Value*);
-
 enum class i2 : char
 {
 };
@@ -150,7 +147,7 @@ struct llvm_value_t
 
 	std::tuple<> match(llvm::Value*& value, llvm::Module*) const
 	{
-		if (peek_through_bitcasts(value) != peek_through_bitcasts(this->value))
+		if (value != this->value)
 		{
 			value = nullptr;
 		}
@@ -506,6 +503,9 @@ using llvm_common_t = std::enable_if_t<(is_llvm_expr_of<T, Types>::ok && ...), t
 template <typename... Args>
 using llvm_match_tuple = decltype(std::tuple_cat(std::declval<llvm_expr_t<Args>&>().match(std::declval<llvm::Value*&>(), nullptr)...));
 
+// Helper function
+llvm::Value* peek_through_bitcasts(llvm::Value*);
+
 template <typename T, typename U = llvm_common_t<llvm_value_t<T>>>
 struct llvm_match_t
 {
@@ -532,7 +532,7 @@ struct llvm_match_t
 
 	std::tuple<> match(llvm::Value*& value, llvm::Module*) const
 	{
-		if (peek_through_bitcasts(value) != peek_through_bitcasts(this->value))
+		if (value != this->value)
 		{
 			value = nullptr;
 		}
